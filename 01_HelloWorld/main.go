9d7d601974e9e6d0a0b12065fc0ab449231d1521
Siavash <svshjalali@gmail.com>
2020-02-07 14:44:50 +0900

    Group import and number of cpus

--- 01_HelloWorld/main.go
+++ 01_HelloWorld/main.go
@@ -1,11 +1,20 @@
+// executable program
 package main
 
-import f "fmt"
+import (
+	"fmt"
+	f "fmt"
+	"runtime"
+)
 
 const ok = true
 
+// main function
 func main() {
 	var hello = "Hello GO!"
 	f.Println(hello, ok)
 	bye()
+
+	// Print number of CPUs
+	fmt.Println("You have:", runtime.NumCPU(), "CPUs")
 }
